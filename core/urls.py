7f6c605bfbfc785cf35cbc5a216a7bb82c16b7ff
Ademar <ademarcandeias@gmail.com>
2023-03-07 12:39:14 -0300

    update

--- core/urls.py
+++ core/urls.py
@@ -13,13 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('core/', include('core.urls'))
 """
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 
-from blog import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('post/new/', views.post_new, name='post_new'),
-    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
+
 ]
